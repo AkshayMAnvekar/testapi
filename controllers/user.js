5016f0845291b213a678f9a0a69f96624f232f6b
AkshayMAnvekar <AkshayMAnvekar@gmail.com>
2020-03-02 01:26:31 +0530

    updates

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then(
@@ -26,6 +27,8 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
+  console.log('test',req.body);
+  
   User.findOne({ email: req.body.email }).then(
     (user) => {
       if (!user) {
